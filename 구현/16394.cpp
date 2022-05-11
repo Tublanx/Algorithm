43413bbcc7ae92441e1df15aed0efe32ca440bbb
Tublanx <rlgus03453@naver.com>
2022-05-11 09:01:50 +0900

    implementation

---
+++ 구현/16394.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int year;
+	
+	cin >> year;
+
+	cout << year - 1946;
+	
+	return 0;
+}
